49b486f8bf17fa115ca2ffda38efdbb66e53d60d
aalmiray <aalmiray@832c1c66-4827-0410-8465-ccb17913cd09>
2009-10-24 02:16:32 +0000

    grails-rest: removed bogus dependencies.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-rest/trunk@56732 832c1c66-4827-0410-8465-ccb17913cd09

--- RestGrailsPlugin.groovy
+++ RestGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import java.lang.reflect.InvocationTargetException
  */
 class RestGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1.1 > *"
     // the other plugins this plugin depends on
