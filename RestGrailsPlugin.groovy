67784ab968864037860f782245e1147bcd8788e0
aalmiray <aalmiray@832c1c66-4827-0410-8465-ccb17913cd09>
2010-02-03 05:24:06 +0000

    grails-rest: added reload options
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-rest/trunk@59133 832c1c66-4827-0410-8465-ccb17913cd09

--- RestGrailsPlugin.groovy
+++ RestGrailsPlugin.groovy
@@ -29,7 +29,7 @@ class RestGrailsPlugin {
     // the plugin version
     def version = "0.3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
+    def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
