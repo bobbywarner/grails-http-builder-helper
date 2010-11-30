93dddb212de23f0c121b5c5a5b566f86fed8b9e5
aalmiray <aalmiray@832c1c66-4827-0410-8465-ccb17913cd09>
2010-11-30 23:24:20 +0000

    grails-rest:
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-rest/trunk@66685 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,12 +12,14 @@ grails.project.dependency.resolution = {
         grailsHome()
         // grailsCentral()
         mavenCentral()
-        mavenRepo name: 'Sonatype', root: 'https://repository.sonatype.org/content/groups/public', m2compatible: true
+        // mavenRepo name: 'Sonatype', root: 'https://repository.sonatype.org/content/groups/public', m2compatible: true
     }
     dependencies {
-        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0-RC2') {exclude 'xercesImpl'}
-        compile('xerces:xercesImpl:2.9.1') { exclude 'xml-apis'}
-        runtime('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0-RC2') {exclude 'xercesImpl'}
-        runtime('xerces:xercesImpl:2.9.1') { exclude 'xml-apis'}
+        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0') {
+            excludes "commons-logging", "xml-apis", "groovy"
+        }
+        // compile('xerces:xercesImpl:2.9.1') { exclude 'xml-apis'}
+        // runtime('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0-RC2') {exclude 'xercesImpl'}
+        // runtime('xerces:xercesImpl:2.9.1') { exclude 'xml-apis'}
     }
 }
