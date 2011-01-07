2b6de59c66f5166ae4aa3b3263373dc4ea45a097
berngp <berngp@832c1c66-4827-0410-8465-ccb17913cd09>
2011-01-07 20:04:10 +0000

    Moving version from 0.6-M1 to 0.6
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-rest/trunk@67906 832c1c66-4827-0410-8465-ccb17913cd09

--- RestGrailsPlugin.groovy
+++ RestGrailsPlugin.groovy
@@ -33,7 +33,7 @@ import org.grails.plugins.rest.ssl.SimpleHTTPBuilderSSLHelper
  */
 class RestGrailsPlugin {
   // the plugin version
-  def version = "0.6-M1"
+  def version = "0.6"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.2.0 > *"
   // the other plugins this plugin depends on
