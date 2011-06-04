205a131fc2574a741505f8cc133aaefa82029ac7
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2011-06-04 01:16:37 -0700

    Upgrading httpbuilder to 0.5.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,17 +5,18 @@ grails.project.test.reports.dir = "target/test-reports"
 
 
 grails.project.dependency.resolution = {
-    inherits('global') 
+    inherits('global')
     log 'warn'
     repositories {
         grailsPlugins()
         grailsHome()
         // grailsCentral()
         mavenCentral()
+        mavenRepo name: 'Codehaus', root: 'http://repository.codehaus.org', m2compatible: true
         // mavenRepo name: 'Sonatype', root: 'https://repository.sonatype.org/content/groups/public', m2compatible: true
     }
     dependencies {
-        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0') {
+        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.1') {
             excludes "commons-logging", "xml-apis", "groovy"
         }
         // compile('xerces:xercesImpl:2.9.1') { exclude 'xml-apis'}
